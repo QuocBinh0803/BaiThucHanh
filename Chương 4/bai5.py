8ae1112b9bc03187590c9dce14993ea55beab6b0
QuocBinh0803 <156582276+QuocBinh0803@users.noreply.github.com>
2024-03-26 23:09:40 +0700

    Update bai5.py

--- Chương 4/bai5.py
+++ Chương 4/bai5.py
@@ -10,7 +10,6 @@ class HanoiTower:
         print(f"Di chuyển đĩa {n} từ tháp {from_rod} đến tháp {to_rod}")
         self.move_disks(n-1, aux_rod, to_rod, from_rod)
 
-# Sử dụng
 num_disks = int(input("Nhập số đĩa: "))
 hanoi = HanoiTower(num_disks)
 print("Các bước di chuyển:")
